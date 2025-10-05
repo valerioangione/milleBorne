e01849c084804c4e8040f82c3158a15edc2b0cc9
valerioangione <valerio.angione@hotmail.fr>
2025-10-05 17:05:42 +0200

    fin tp2

---
+++ src/jeu/Jeu.java
@@ -0,0 +1,23 @@
+package jeu;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import cartes.*;
+import utils.GestionCartes;
+
+public class Jeu {
+	private Sabot sabot;
+	
+	public Jeu() {
+		JeuDeCartes jeu = new JeuDeCartes();
+		List<Carte> listeCarteNonMelangee = new LinkedList<>();
+        for (Carte carte : jeu.donnerCartes()) {
+        listeCarteNonMelangee.add(carte);
+        }
+        List<Carte> listeCartes = new ArrayList<>(listeCarteNonMelangee);
+        listeCartes = GestionCartes.melanger(listeCartes);
+        sabot =new Sabot( (Carte[]) listeCartes.toArray());
+	}
+}
